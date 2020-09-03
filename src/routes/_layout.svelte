90c8b00c457b66421f1a4bcadb7144f934151c33
erianvc <erianvc.dev@gmail.com>
2020-09-03 15:46:08 -0500

    feat(core): add hero segment

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,3 @@
 <main>
     <slot />
 </main>
-
-<style>
-    main {
-        @apply grid min-h-screen;
-        place-items: center;
-    }
-</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,55 +1,5 @@
 <script>
-    import Button from '@components/Button.svelte'
-    import Text from '@components/Text.svelte'
-    import Icon from '@components/Icon.svelte'
+    import Hero from '@segments/Hero.svelte'
 </script>
 
-<section class="container flex justify-center">
-    <Icon center name="logo" class="h-40"/>
-</section>
-
-<section class="container flex flex-wrap justify-center">
-    <Text class="w-full lg:w-1/2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloribus quae iste et.
-        Hic tempora eveniet voluptatibus inventore placeat optio quae impedit sed adipisci repellat?
-        Enim perspiciatis amet praesentium minus.
-    </Text>
-
-    <Text right class="w-full lg:w-1/2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloribus quae iste et.
-        Hic tempora eveniet voluptatibus inventore placeat optio quae impedit sed adipisci repellat?
-        Enim perspiciatis amet praesentium minus.
-    </Text>
-
-    <Text class="w-full lg:w-1/2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloribus quae iste et.
-        Hic tempora eveniet voluptatibus inventore placeat optio quae impedit sed adipisci repellat?
-        Enim perspiciatis amet praesentium minus.
-    </Text>
-
-    <Text left class="w-full lg:w-1/2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloribus quae iste et.
-        Hic tempora eveniet voluptatibus inventore placeat optio quae impedit sed adipisci repellat?
-        Enim perspiciatis amet praesentium minus.
-    </Text>
-</section>
-
-<section class="flex flex-col space-y-4">
-    <div>
-        <Button featured fill>
-            Button Prime fill
-        </Button>
-        <Button fill>
-            Button Fill
-        </Button>
-    </div>
-
-    <div>
-        <Button featured outline>
-            Button Prime outline
-        </Button>
-        <Button outline>
-            Button outline
-        </Button>
-    </div>
-</section>
+<Hero />
