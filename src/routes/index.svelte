07697847e4a7bf55ee546b4481d62871359c571f
erianvc <erianvc.dev@gmail.com>
2020-09-03 12:06:41 -0500

    feat: text component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
     import Button from '@components/Button.svelte'
+    import Text from '@components/Text.svelte'
 </script>
 
 <section class="flex flex-col space-y-4">
@@ -20,4 +21,30 @@
             Button outline
         </Button>
     </div>
+</section>
+
+<section class="container flex flex-wrap justify-center">
+    <Text class="w-full lg:w-1/2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloribus quae iste et.
+        Hic tempora eveniet voluptatibus inventore placeat optio quae impedit sed adipisci repellat?
+        Enim perspiciatis amet praesentium minus.
+    </Text>
+
+    <Text right class="w-full lg:w-1/2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloribus quae iste et.
+        Hic tempora eveniet voluptatibus inventore placeat optio quae impedit sed adipisci repellat?
+        Enim perspiciatis amet praesentium minus.
+    </Text>
+
+    <Text class="w-full lg:w-1/2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloribus quae iste et.
+        Hic tempora eveniet voluptatibus inventore placeat optio quae impedit sed adipisci repellat?
+        Enim perspiciatis amet praesentium minus.
+    </Text>
+
+    <Text left class="w-full lg:w-1/2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia doloribus quae iste et.
+        Hic tempora eveniet voluptatibus inventore placeat optio quae impedit sed adipisci repellat?
+        Enim perspiciatis amet praesentium minus.
+    </Text>
 </section>
