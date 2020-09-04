64972c70c3323f0de8873c645244b8ba578b85d8
erianvc <erianvc.dev@gmail.com>
2020-09-03 19:28:09 -0500

    feat: image component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,13 @@
 <script>
     import Hero from '@segments/Hero.svelte'
+    import Image from '@components/Image.svelte'
 </script>
 
 <Hero />
+
+<section class="grid grid-cols-2 grid-rows-2 gap-8">
+    <Image src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-iasdsadasdmage-size.pasdasng"/>
+    <Image />
+    <Image />
+    <Image />
+</section>
