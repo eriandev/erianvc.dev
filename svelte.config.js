1a7b7adc154dee4a8a0e470722ad59f013e9d43e
erianvc <erianvc.dev@gmail.com>
2020-09-05 09:58:20 -0500

    chore: config to use Svelte with Tailwind

---
+++ svelte.config.js
@@ -0,0 +1,10 @@
+const postcss = require('./postcss.config')
+const defaults = {
+    style: 'postcss',
+}
+
+module.exports = {
+    preprocess: [ 
+        sveltePreprocess({ defaults, postcss }),
+    ],
+}
