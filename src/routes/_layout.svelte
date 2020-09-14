dae20b6457e55f95f2ec81603521a146e254f839
erianvc <erianvc.dev@gmail.com>
2020-09-14 00:30:37 -0500

    feat: navbar markup complete

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,9 @@
+<script>
+    import Navbar from '@components/Navbar.svelte'
+</script>
+
 <main>
     <slot />
 </main>
+
+<Navbar />
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
         screens: {
             sm: '600px',
             md: '768px',
-            lg: '1280px',
+            lg: '1024px',
             xl: '1440px',
             '2xl': '1900px',
         },
