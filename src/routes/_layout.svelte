68aa1fe9896ef700563199bbbcf6e961cee67bdf
erianvc <erianvc.dev@gmail.com>
2020-09-24 19:37:31 -0500

    feat(core): add head component

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,8 +1,11 @@
 <script>
+    import Head from '@components/Head.svelte'
     import Navbar from '@components/Navbar.svelte'
 
     export let segment
 </script>
+
+<Head />
 
 <main>
     <slot />
